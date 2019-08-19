e7a82d3a8fe4c26d490971f76a9c0136e67c04bb
pifernandez <pilfernandez1@gmail.com>
2019-08-19 01:12:27 -0300

    modal

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const openModal = () => {
+    let container = document.getElementById('modal')
+    container.classList.remove('close-modal')
+    container.classList.add('modal-container')
+}
+
+const closeModal = () => {
+    let container = document.getElementById('modal')
+    container.classList.remove('modal-container')
+    container.classList.add('close-modal')
+}
+
