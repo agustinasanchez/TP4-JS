dced5702dbd76454a7126f7782390714827c0c63
pifernandez <pilfernandez1@gmail.com>
2019-08-19 22:25:05 -0300

    listas y api

--- js/main.js
+++ js/main.js
@@ -13,6 +13,52 @@ const closeModal = () => {
 }
 
 // Api
+// POPULAR
+
+fetch(`https://api.themoviedb.org/3/movie/popular?api_key=de8e683780427ec48ccb17461ebf36c3`)
+  .then(response => response.json())
+  .then( res => res.results.forEach(e => console.log(e)))
+
+fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=de8e683780427ec48ccb17461ebf36c3`)
+  .then(response => response.json())
+  .then( res => res.results.forEach(e => console.log(e)))
+
+fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=de8e683780427ec48ccb17461ebf36c3`)
+  .then(response => response.json())
+  .then( res => res.results.forEach(e => console.log(e)))
+
+fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=de8e683780427ec48ccb17461ebf36c3`)
+  .then(response => response.json())
+  .then( res => res.results.forEach(e => console.log(e)))
+
+// Creo en el DOM la lista
+
+const categoryMovie = (id) => {
+  let container = document.getElementById(id)
+
+  createElem('ul', 'movies-list', container)
+  createElem('li', 'movie', ul)
+
+  let a = document.createElement('a')
+  a.href('#')
+  // a.onclick(openModal())
+  li.appendChild(a)
+
+  createElem('div', 'movie-img', a)
+  createElem('img', 'img', div)
+  createElem('h3', 'movie-title', a)
+}
+
+const createElem = (elem, className, container) => {
+  let name = document.createElement(elem)
+  name.classList.add(className)
+  container.appendChild(name)
+}
+
+categoryMovie('popular')
+categoryMovie('top-rates')
+categoryMovie('upcoming')
+categoryMovie('now-playing')
 
 
 
