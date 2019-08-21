951d884a9e1dd7820c09628c5d285c5eead2b136
pifernandez <pilfernandez1@gmail.com>
2019-08-21 13:48:16 -0300

    wip

--- js/main.js
+++ js/main.js
@@ -15,50 +15,85 @@ const closeModal = () => {
 // Api
 // POPULAR
 
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=de8e683780427ec48ccb17461ebf36c3`)
-  .then(response => response.json())
-  .then( res => res.results.forEach(e => console.log(e)))
+let apiKey = `de8e683780427ec48ccb17461ebf36c3`
+let categories = ['popular', 'top_rated', 'upcoming', 'now_playing']
+let idCategories = ['popular', 'top-rated', 'upcoming', 'now-playing']
 
-fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=de8e683780427ec48ccb17461ebf36c3`)
-  .then(response => response.json())
-  .then( res => res.results.forEach(e => console.log(e)))
 
-fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=de8e683780427ec48ccb17461ebf36c3`)
-  .then(response => response.json())
-  .then( res => res.results.forEach(e => console.log(e)))
-
-fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=de8e683780427ec48ccb17461ebf36c3`)
-  .then(response => response.json())
-  .then( res => res.results.forEach(e => console.log(e)))
-
-// Creo en el DOM la lista
-
-const categoryMovie = (id) => {
-  let container = document.getElementById(id)
-
-  createElem('ul', 'movies-list', container)
-  createElem('li', 'movie', ul)
-
-  let a = document.createElement('a')
-  a.href('#')
-  // a.onclick(openModal())
-  li.appendChild(a)
-
-  createElem('div', 'movie-img', a)
-  createElem('img', 'img', div)
-  createElem('h3', 'movie-title', a)
-}
-
-const createElem = (elem, className, container) => {
+const createElem = (elem, className) => {
   let name = document.createElement(elem)
   name.classList.add(className)
-  container.appendChild(name)
+  return name
 }
 
-categoryMovie('popular')
-categoryMovie('top-rates')
-categoryMovie('upcoming')
-categoryMovie('now-playing')
+const fetchFunction = (category, idCategory) => fetch(`https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(res => {
+      createList(res.results[0], idCategory)
+      createList(res.results[1], idCategory)
+      createList(res.results[2], idCategory)
+      createList(res.results[3], idCategory)
+  })
 
+fetchFunction(categories[0],idCategories[0])
+fetchFunction(categories[1],idCategories[1])
+fetchFunction(categories[2],idCategories[2])
+fetchFunction(categories[3],idCategories[3])
 
+const createList = ({title, id, poster_path}, idContainer) => {
+  let container = document.getElementById(idContainer)
+
+  let li = createElem('li', 'movie')
+  li.id = id
+  container.appendChild(li)
+  let a = createElem('a', 'link')
+  a.href = '#'
+  a.addEventListener('click', openModal, true)
+  li.appendChild(a)
+  let div = createElem('div', 'movie-img')
+  a.appendChild(div)
+  let img = createElem('img', 'img')
+  img.value = poster_path
+  div.appendChild(img)
+  let h3 = createElem('h3', 'movie-title')
+  h3.innerText = title
+  a.appendChild(h3)
+}
+
+const fillModal = ({title, tagline, poster_path, backdrop_path, overview, release_date}) => {
+  fillElem('movie-title', title)
+  fillElem('overview', overview)
+  fillElem('sub-title', tagline)
+  fillElem('header-img', backdrop_path)
+  fillElem('movie-image', poster_path)
+  fillElem('release-date', release_date)
+  // genres.forEach( e => {
+  //   fillElem('genres', `${e.name}, `)
+  // })
+}
+
+const fillElem = (idElem, content) => {
+  let elem = document.getElementById(idElem)
+  elem.innerText = content
+}
+
+const fetchMovie = (peliculaId, apiKey) => fetch(`https://api.themoviedb.org/3/movie/${peliculaId}?api_key=${apiKey}`)
+  .then(response => response.json())
+  .then(res => fillModal(res))
+
+fetchMovie('420818', apiKey)
+
+const popularPage = ({results}) => {
+  let home = document.getElementsById('home')
+  home. innerHTML = ' '
+  createList(results[0], 'home')
+}
+
+const fetchPage = (apiKey, page) => {
+  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${page}`)
+    .then(response => response.json())
+    .then(res => popularPage(res))
+}
+
+fetchPage(apiKey, 1)
 
